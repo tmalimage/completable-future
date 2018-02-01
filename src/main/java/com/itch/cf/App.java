37d03d6d9f12070774a17b517bb224f7baf0f72c
vmalith <tharindu.malimage@pearson.com>
2018-02-02 00:00:13 +0530

    initial set-up

---
+++ src/main/java/com/itch/cf/App.java
@@ -0,0 +1,13 @@
+package com.itch.cf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
